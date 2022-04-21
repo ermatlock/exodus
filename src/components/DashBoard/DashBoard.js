d9d0acf6b1c6704c3352e08db73a346ec4f732d4
Eric Matlock <ermatlock@gmail.com>
2022-04-21 11:14:11 -0400

    Create DashBoard component

---
+++ src/components/DashBoard/DashBoard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./DashBoard.css"
+
+const DashBoard = () => {
+  return (
+    <div className='dash-board-container'>
+      <h1>I am the Dashboard!</h1>
+    </div>
+  )
+}
+export default DashBoard 
