86e7b9f2affb0e9332382e8c7e86eef636c15e9c
Eric Matlock <ermatlock@gmail.com>
2022-04-21 18:25:19 -0400

    Layout componet structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,19 @@ import "./App.css";
 import { DataContextProvider } from "../../contexts/DataContext";
 import DashBoard from "../DashBoard/DashBoard"
 import Loader from "../Loader/Loader"
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
+
 
 const App = () => {
   return (
     <div className="App">
-    <Loader />
+    <Header />
+    {/* <Loader /> */}
     <DataContextProvider>
       <DashBoard />
     </DataContextProvider>
+    <Footer />
     </div>
   );
 }
