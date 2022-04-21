d5d9c59e0e3e5dde53a2c97d2d0b66464f34d2e7
Eric Matlock <ermatlock@gmail.com>
2022-04-21 18:31:54 -0400

    Update Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>This is the footer</h2>
+    </footer>
+  )
+}
+
+export default Footer
