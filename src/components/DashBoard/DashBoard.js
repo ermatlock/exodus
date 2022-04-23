2b720929d6d2e86c3520472189cf1b4a4a8a671a
Eric Matlock <ermatlock@gmail.com>
2022-04-23 14:45:38 -0400

    creat cityCards function

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import "./DashBoard.css"
+import React, { useContext, useEffect } from "react";
+import "./DashBoard.css";
+import Loader from "../Loader/Loader";
+import { nanoid } from "nanoid";
+import FiCameraOff from 'react-icons/fa';
+import CityCard from "../CityCard/CityCard"
+import { cleanCityCardData } from "../../utils";
 
-const DashBoard = () => {
+const DashBoard = ({citiesList}) => {
+  const cityCards = citiesList.map(city => {
+    let cleanedCity = cleanCityCardData(city)
+    return (
+      // <Link to={`/${city.id}`} key={city.id} style={{ textDecoration: 'none' }}>
+      <CityCard
+        id={cleanedCity.id}
+        key={cleanedCity.id}
+        image={cleanedCity.image}
+        name={cleanedCity.name}
+        lgtbqScore={cleanedCity.lgbtqScore}
+        minoritizedScore={cleanedCity.minoritizedScore}
+      />
+    )  
+      // </Link>
+  })
+
   return (
-    <div className='dash-board-container'>
-      <h1>I am the Dashboard!</h1>
-    </div>
-  )
-}
-export default DashBoard 
+    <section className="dash-board-container">
+      {cityCards}
+    </section>
+  );
+};
+
+export default DashBoard;
