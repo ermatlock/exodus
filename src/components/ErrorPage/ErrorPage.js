e999209e2ef1aff8ce0c743661620448f407ae80
Eric Matlock <ermatlock@gmail.com>
2022-04-24 18:03:58 -0400

    Autoformat

---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./ErrorPage.css"
+
+const ErrorPage = () => {
+  return (
+    <div className="error-container">
+      <h1>Sorry, We're having trouble getting that. Please try again!</h1>
+    </div>
+  )
+}
+
+export default ErrorPage
