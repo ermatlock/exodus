02e08b668a719b7ae3264ec45cc4f3418f7f5a63
Eric Matlock <ermatlock@gmail.com>
2022-04-24 18:04:26 -0400

    Build out favorites structure

---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+import "./Favorites.css"
+
+class Favorites extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+ render () {
+   return (
+     <div className="favorites-container">
+
+     </div>
+   )
+ }
+}
+
+export default Favorites
