49faa0a40bc91778f060a53af0fe103d5ff6f648
Eric Matlock <ermatlock@gmail.com>
2022-04-25 12:50:09 -0400

    Add testing

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -114,7 +114,7 @@ it('As a user, I should be able to select a city from the dropdown menu', () => 
     fixture: 'cityImages.json'
   }).as("matchedUrl")
   cy.visit('http://localhost:3000/')
-  .get('select').get('option').contains('Austin')
+  .get('select').select('denver').should('have.value', 'denver')
   .should('be.visible')
 });
 
@@ -136,12 +136,56 @@ it('As a user, I should be able to click the "go to favorites" button to go to t
     fixture: 'cityImages.json'
   }).as("matchedUrl")
   cy.visit('http://localhost:3000/')
-  .get('select').get('option').contains('Austin')
+  .get('.favorites-switch').contains('Go To Favorites')
+  .get('.favorites-switch').click()
+  .get('.favorites-switch').contains('Back To Main')
+  .get('.favorites-switch').click()
   .should('be.visible')
 });
 
+it('As a user, I should see a widget with LGBT Rights', () => {
+  cy.intercept('GET', "https://api.teleport.org/api/urban_areas/slug:buffalo/", {
+      statusCode: 200,
+      fixture: 'cityData.json'
+    }).as('matchedUrl')
+  cy.intercept('GET', 'https://api.teleport.org/api/urban_areas/slug:buffalo/scores', {
+    statusCode: 200,
+    fixture: 'cityScores.json'
+  }).as("matchedUrl")
+  cy.intercept('GET', 'https://api.teleport.org/api/urban_areas/slug:buffalo/details', {
+    statusCode: 200,
+    fixture: 'cityDetails.json'
+  }).as("matchedUrl")
+  cy.intercept('GET', 'https://api.teleport.org/api/urban_areas/slug:buffalo/images/', {
+    statusCode: 200,
+    fixture: 'cityImages.json'
+  }).as("matchedUrl")
+  cy.visit('http://localhost:3000/')
+  .get('.embed-detail').should('be.visible')
+  
+});
 
-
+it('As a user, if the server is down, I will get an error message', () => {
+  cy.intercept('GET', "https://api.teleport.org/api/urban_areas/slug:buffalo/", {
+      statusCode: 400,
+      ok: false
+    }).as('matchedUrl')
+  cy.intercept('GET', 'https://api.teleport.org/api/urban_areas/slug:buffalo/scores', {
+    statusCode: 400,
+      ok: false
+  }).as("matchedUrl")
+  cy.intercept('GET', 'https://api.teleport.org/api/urban_areas/slug:buffalo/details', {
+    statusCode: 400,
+      ok: false
+  }).as("matchedUrl")
+  cy.intercept('GET', 'https://api.teleport.org/api/urban_areas/slug:buffalo/images/', {
+    statusCode: 400,
+      ok: false
+  }).as("matchedUrl")
+  cy.visit('http://localhost:3000/')
+  .get('h1[class=".error-container"]')
+  .contains("Sorry, We're having trouble getting that. Please try again!")
+});
 
 
 });
