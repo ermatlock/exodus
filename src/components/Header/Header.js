e2b88859c0b2d0ac4f165e59649256b619a30634
Eric Matlock <ermatlock@gmail.com>
2022-04-25 04:54:37 -0400

    Add link buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,35 @@
 import React from "react";
 import "./Header.css";
 import SearchBar from "../SearchBar/SearchBar";
-import { Link } from "react-router-dom";
+import logo from "../../images/exodus.png"
+
+import { Link, useLocation } from "react-router-dom";
 
 const Header = ({ id, changeId }) => {
-  
+  const location = useLocation();
 
   return (
     <header>
-      <h2>Exodus</h2>
-      <SearchBar
+    <Link to="/" >
+      <img className="logo" src={logo} alt="exodus logo"/>
 
-      id={id}
-      changeId={changeId}
-
-      />
-      <Link to="/favorites">
-        <button>Go To Favorites</button>
-      </Link>
+    </Link>
+      <>
+        {location.pathname === "/" ? (
+          <SearchBar id={id} changeId={changeId} />
+        ) : null}
+      </>
+      <>
+        {location.pathname === "/" ? (
+          <Link to="/favorites">
+            <button className="favorites-switch">Go To Favorites</button>
+          </Link>
+        ) : (
+          <Link to="/">
+            <button className="favorites-switch"> Back To Main </button>
+          </Link>
+        )}
+      </>
     </header>
   );
 };
